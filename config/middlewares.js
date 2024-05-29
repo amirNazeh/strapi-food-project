7ec9f6a1a0664d393723d234a353efaddf158adc
amirNazeh <130168914+amirNazeh@users.noreply.github.com>
2024-05-29 19:41:52 +0300

    Add files via upload

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,20 @@
+// config/middleware.js
 module.exports = [
-  "strapi::logger",
+  // Middleware الافتراضية
   "strapi::errors",
   "strapi::security",
   "strapi::cors",
   "strapi::poweredBy",
+  "strapi::logger",
   "strapi::query",
   "strapi::body",
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
+
+  // إضافة الـ Middleware المخصصة
+  // {
+  //   name: "disableRegisterIfNotLoggedIn",
+  //   resolve: "./middlewares/disableRegisterIfNotLoggedIn",
+  // },
 ];
--- config/middlewares/disableRegisterIfNotLoggedIn.js
+++ config/middlewares/disableRegisterIfNotLoggedIn.js
@@ -1,8 +1,14 @@
-module.exports = async (ctx, next) => {
-  if (ctx.request.path === "/admin/auth/register" && !ctx.state.admin) {
-    ctx.status = 403;
-    ctx.body = "Register route is disabled unless logged in";
-  } else {
-    await next();
-  }
-};
+// module.exports = (strapi) => {
+//   return {
+//     initialize() {
+//       return async (ctx, next) => {
+//         if (ctx.request.path === "/admin/auth/register" && !ctx.state.admin) {
+//           ctx.status = 403;
+//           ctx.body = "Register route is disabled unless logged in";
+//         } else {
+//           await next();
+//         }
+//       };
+//     },
+//   };
+// };
