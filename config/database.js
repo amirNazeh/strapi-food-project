1bbbc714f8396bba9d9119618b32bb0d554d3fb7
amirNazeh <130168914+amirNazeh@users.noreply.github.com>
2024-05-29 15:20:06 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,22 @@ const path = require('path');
 
 module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
-
+defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {},
+    },
+  },
   const connections = {
     mysql: {
       connection: {
