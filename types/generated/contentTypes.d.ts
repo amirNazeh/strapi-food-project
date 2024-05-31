42f0c3d27274ac9da532f6ebb24f4576c78bc005
amirNazeh <130168914+amirNazeh@users.noreply.github.com>
2024-05-31 06:38:15 +0300

    Add files via upload

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -375,8 +375,8 @@ export interface ApiFoodFood extends Schema.CollectionType {
   };
   attributes: {
     name: Attribute.String;
-    image: Attribute.Media;
     descrition: Attribute.Text;
+    image: Attribute.Text;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
